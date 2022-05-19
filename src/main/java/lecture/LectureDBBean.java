74a55a501a42c85b5d61ff1a8c675673c66b03a8
10e5 <jej10e5@gmail.com>
2022-05-19 11:43:44 +0900

    signup

--- src/main/java/lecture/LectureDBBean.java
+++ src/main/java/lecture/LectureDBBean.java
@@ -1,6 +1,12 @@
 package lecture;
 
+import lecmem.LecmemDataBean;
+
 public class LectureDBBean implements LectureDao{
+	@Override
+	public int insertMember( LecmemDataBean dto ) {		
+		return SqlMapClient.getSession().insert( "Lecture.insertMember", dto );
+	}
 	@Override
 	public int createClass(LectureDataBean dto) {
 		return SqlMapClient.getSession().
--- src/main/java/lecture/LectureDao.java
+++ src/main/java/lecture/LectureDao.java
@@ -1,5 +1,8 @@
 package lecture;
 
+import lecmem.LecmemDataBean;
+
 public interface LectureDao {
 	public int createClass(LectureDataBean dto);
+	public int insertMember( LecmemDataBean dto );
 }
