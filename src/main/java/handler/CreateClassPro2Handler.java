6668a0e06ab2ae06a40d08287dac3598f9581cca
10e5 <jej10e5@gmail.com>
2022-05-19 17:13:33 +0900

    다음api

---
+++ src/main/java/handler/CreateClassPro2Handler.java
@@ -0,0 +1,18 @@
+package handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CreateClassPro2Handler implements CommandHandler{
+	@RequestMapping("/createClassPro2")
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return new ModelAndView("class365/createClassPro2");
+	}
+}
